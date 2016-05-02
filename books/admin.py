c05ecc469641746f0960ed8dce5dab574f42e427
hirosh7 <hirosh7@gmail.com>
2016-05-01 18:09:40 -0600

    Turbo Charging Our Admin changes

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 from .models import Book, Author
 
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Book Detail", {"fields": ["title", "authors"]}),
+        ("Review", {"fields": ["is_favorite", "review", "date_reviewed"]})
+    ]
+
+    readonly_fields = ("date_reviewed",)
+
+    def book_authors(self, obj):
+        return obj.list_authors()
+
+    book_authors.short_description = "Author(s)"
+    list_editable = ("is_favorite",)
+    list_filter = ("is_favorite",)
+    search_fields = ("title", "authors__name",)
+
+    list_display = ("title", "book_authors", "date_reviewed", "is_favorite",)
 
 # Register your models here.
-admin.site.register(Book)
 admin.site.register(Author)
